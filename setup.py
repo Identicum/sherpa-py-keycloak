3256da57119793a60a6af903059b525d5fcb4280
Gustavo J Gallardo <ggallard@identicum.com>
2025-01-28 13:00:55 -0300

    version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='sherpa-py-keycloak',
-    version='1.0.20241007',
+    version='1.0.20250128',
     description='Python utilities for Keycloak',
     url='git@github.com:Identicum/sherpa-py-keycloak.git',
     author='Identicum',
